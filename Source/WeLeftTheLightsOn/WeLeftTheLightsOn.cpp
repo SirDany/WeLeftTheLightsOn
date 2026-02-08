9ad15cb9d67f193c29a1acd7352921ea50488aec
Sir Dany <prototype-dany@proton.me>
2026-02-08 17:19:35 +0100

    WLTLO-56 Init rates
    - Adds base rates to data table
    - Inits base rates with a gameplay effect

--- Source/WeLeftTheLightsOn/WeLeftTheLightsOn.cpp
+++ Source/WeLeftTheLightsOn/WeLeftTheLightsOn.cpp
@@ -1,5 +1,6 @@
 #include "WeLeftTheLightsOn.h"
-
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WeLeftTheLightsOn, "WeLeftTheLightsOn" );
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, WeLeftTheLightsOn, "WeLeftTheLightsOn");
+
+DEFINE_LOG_CATEGORY(LogWeLeftTheLightsOn);
